2c6e845e24033118f857f02ef78f9f3c6359c90f
henriqu3x <163440449+henriqu3x@users.noreply.github.com>
2026-08-06 15:39:44 -0300

    docs(sql): add sql reteste3

---
+++ simulado_01/ReTeste03/query.sql
@@ -0,0 +1,17 @@
+--1v;
+select nome,nome_produto,quantidade, (quantidade * preco_unitario) as valor_total from itens_pedido as ip 
+inner join produtos as p on ip.id_produto = p.id_produto
+inner join pedidos as pe on ip.id_pedido = pe.id_pedido
+inner join clientes as c on pe.id_cliente = c.id_cliente
+where status = 'Concluído' order by nome asc;
+
+--3v
+select p.nome_produto, p.preco, p.id_categoria from produtos as p where p.preco > (select avg(preco) from produtos p2 
+where p2.id_categoria = p.id_categoria) order by id_produto asc
+
+--7v
+left join funcionarios as s on s.id_funcionario = f.id_supervisor
+
+--8v
+select p.nome_produto from avaliacoes as a right join produtos as p on a.id_produto = p.id_produto
+where id_avaliacao is null order by p.id_produto asc
